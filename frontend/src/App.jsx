4b4ab8e4024c2024185d62a546485b6b02a1cceb
vivek sachdev <viveksachdev@viveks-MacBook-Air.local>
2025-01-19 18:29:54 +0530

    frontend pointing to backend svc of k8s instead pointing to localhost backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
   const [newTask, setNewTask] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5001/tasks')
+    fetch('http://backend.taskmanager.svc.cluster.local:5001/tasks')
       .then((response) => response.json())
       .then((data) => setTasks(data));
   }, []);
 
   const addTask = async () => {
     if (!newTask.trim()) return;
-    const response = await fetch('http://localhost:5001/tasks', {
+    const response = await fetch('http://backend.taskmanager.svc.cluster.local:5001/tasks', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title: newTask }),
@@ -23,7 +23,7 @@ function App() {
   };
 
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5001/tasks/${id}`, { method: 'DELETE' });
+    await fetch(`http://backend.taskmanager.svc.cluster.local:5001/tasks/${id}`, { method: 'DELETE' });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
