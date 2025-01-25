b4114c5cb047b1309f9c13174cf50b72da364c58
vivek sachdev <viveksachdev@viveks-MacBook-Air.local>
2025-01-25 20:03:47 +0530

    backend url changes in frontned and db host id change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
   const [newTask, setNewTask] = useState('');
 
   useEffect(() => {
-    fetch('a3fa7814e7a8146ed852e8fe2dcb7028-7477371.us-east-1.elb.amazonaws.com/backend/tasks')
+    fetch('http://a7e5185d1f8c84b90b3904ffe622fb13-1735135714.us-east-1.elb.amazonaws.com/backend/tasks')
       .then((response) => response.json())
       .then((data) => setTasks(data));
   }, []);
 
   const addTask = async () => {
     if (!newTask.trim()) return;
-    const response = await fetch('a3fa7814e7a8146ed852e8fe2dcb7028-7477371.us-east-1.elb.amazonaws.com/backend/tasks', {
+    const response = await fetch('http://a7e5185d1f8c84b90b3904ffe622fb13-1735135714.us-east-1.elb.amazonaws.com/backend/tasks', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title: newTask }),
@@ -23,7 +23,7 @@ function App() {
   };
 
   const deleteTask = async (id) => {
-    await fetch(`a3fa7814e7a8146ed852e8fe2dcb7028-7477371.us-east-1.elb.amazonaws.com/backend/tasks/${id}`, { method: 'DELETE' });
+    await fetch(`http://a7e5185d1f8c84b90b3904ffe622fb13-1735135714.us-east-1.elb.amazonaws.com/backend/tasks/${id}`, { method: 'DELETE' });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
